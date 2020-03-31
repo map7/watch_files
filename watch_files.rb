04f55fbeb86775ffe94861d44da2633ce004fc3c
Michael Pope <michael@dtcorp.com.au>
2020-04-01 09:26:49 +1100

    Rename main file

--- watch_files.rb
+++ watch_files.rb
@@ -7,8 +7,8 @@ require 'rb-inotify'
 # Note: rb-inotify doesn't need to be in a thread, as soon as you setup your watch
 # it is watching the directory. Then at the end you can process all the files which
 # have gone in there after your actions.
-@notifier.watch("/home/tram/work/clt", :moved_to, :create) do |file|
-  puts file.name
+@notifier.watch("/home/tram/work/clt", :recursive, :moved_to, :create) do |file|
+  puts file.absolute_name
 end
 
 @notifier.run
