c32e4702b0fd8bd200acac740599532e44ec80a0
Michael Pope <michael@dtcorp.com.au>
2020-04-01 09:51:25 +1100

    Extract variables to constants

--- watch_files.rb
+++ watch_files.rb
@@ -5,21 +5,24 @@ require 'byebug'
 
 @notifier = INotify::Notifier.new
 
+USER = "t100"
+GROUP = "tram"
+DIR = "/home/tram/work/clt"
+
 # Note: rb-inotify doesn't need to be in a thread, as soon as you setup your watch
 # it is watching the directory. Then at the end you can process all the files which
 # have gone in there after your actions.
-@notifier.watch("/home/tram/work/clt", :recursive, :moved_to, :create) do |file|
+@notifier.watch(DIR, :recursive, :moved_to, :create) do |file|
   puts "Changing permissions of #{file.absolute_name}"
-  
+
   if File.file?(file.absolute_name)
-    `chown t100:tram #{file.absolute_name}`
+    `chown #{USER}:#{GROUP} #{file.absolute_name}`
     `chmod g+w #{file.absolute_name}`
   else
-    `chown -R t100:tram #{file.absolute_name}`
+    `chown -R #{USER}:#{GROUP} #{file.absolute_name}`
     `chmod g+x #{file.absolute_name}`
     `chmod -R g+w #{file.absolute_name}`
   end
 end
 
 @notifier.run
-
