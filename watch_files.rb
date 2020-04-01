b6ffb4d2a7f44dba58f10bdada5093b60eed1844
Michael Pope <michael@dtcorp.com.au>
2020-04-01 15:09:56 +1100

    Tidy code

--- watch_files.rb
+++ watch_files.rb
@@ -1,28 +1,23 @@
 #!/usr/bin/env ruby
-
 require 'rb-inotify'
 require 'fileutils'
 require 'yaml'
-require 'byebug'
-
-@notifier = INotify::Notifier.new
 
 config=YAML.load(File.read(File.expand_path("~/.watch_files.yaml")))
-USER = config[:user]
-GROUP = config[:group]
-DIR = config[:dir]
+
+@notifier = INotify::Notifier.new
 
 # Note: rb-inotify doesn't need to be in a thread, as soon as you setup your watch
 # it is watching the directory. Then at the end you can process all the files which
 # have gone in there after your actions.
-@notifier.watch(DIR, :recursive, :moved_to, :create) do |file|
+@notifier.watch(config[:dir], :recursive, :moved_to, :create) do |file|
   puts "Changing permissions of #{file.absolute_name}"
 
   if File.file?(file.absolute_name)
     FileUtils.chmod "g=rw,o=", file.absolute_name, verbose: true
-    FileUtils.chown nil, GROUP, file.absolute_name, verbose: true
+    FileUtils.chown nil, config[:group], file.absolute_name, verbose: true
   else
-    `chown :tram -R #{file.absolute_name}`
+    `chown :#{config[:group]} -R #{file.absolute_name}`
     `chmod g+x #{file.absolute_name}`
     `chmod -R g+w #{file.absolute_name}`
   end
